b5a5e2f9dad64993aff29057b5efa06acf17cd5b
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2026-03-19 06:58:51 +0530

    Initial commit

---
+++ src/main/java/com/dcj/ecommerce_platform_be/EcommercePlatformBeApplication.java
@@ -0,0 +1,14 @@
+package com.dcj.ecommerce_platform_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePlatformBeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(EcommercePlatformBeApplication.class, args);
+	}
+
+}
