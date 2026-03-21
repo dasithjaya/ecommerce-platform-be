42e53116031676f0179ea18cc987e7ee4ee8f5a8
dasithjaya <2020cs081@stu.ucsc.cmb.ac.lk>
2026-03-21 16:07:41 +0530

    Implement image handling features

--- src/main/java/com/dcj/ecommerce_platform_be/model/Product.java
+++ src/main/java/com/dcj/ecommerce_platform_be/model/Product.java
@@ -1,10 +1,7 @@
 package com.dcj.ecommerce_platform_be.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,6 +26,11 @@ public class Product {
     private Date releaseDate;
     private boolean isAvailable;
     private int quantity;
+    private String imageName;
+    private String imageType;
+    // @Lob -> Large Object
+    @Lob
+    private byte[] imageData;
 
     public Product(int i) {
         this.productId = i;
